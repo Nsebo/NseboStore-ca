69c40bf88c7a4e8b2aa85cc208183bd5c0a2c7c7
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-02 16:35:42 +0200

    adds final fixes to the layouts folders

--- src/layouts/Button/Button.jsx
+++ src/layouts/Button/Button.jsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
 const Button = ({ product, btnText, orange, black, transparent, to }) => {
-  return (
-    <Link
-      className={`${styles.button} ${orange ? styles.orange : ''} ${
-        black ? styles.black : ''
-      } ${transparent ? styles.transparent : ''} `}
-      to={to ? product : ''}
-    >
-      {btnText}
-    </Link>
-  );
+    return (
+        <Link
+            className={`${styles.button} ${orange ? styles.orange : ''} ${
+                black ? styles.black : ''
+            } ${transparent ? styles.transparent : ''} `}
+            to={to ? product : ''}
+        >
+            {btnText}
+        </Link>
+    );
 };
 
 export default Button;
